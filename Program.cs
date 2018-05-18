d446a098edef25cc7ffcf54178a18a4fc6312f3b
codestrengthen <33879202+codestrengthen@users.noreply.github.com>
2018-05-18 10:06:45 +1000

    Start week 2

--- Program.cs
+++ Program.cs
@@ -10,24 +10,7 @@ namespace LINQDemo
     {
         static void Main(string[] args)
         {
-            using (var db  = new RestaurantContext())
-            {
-                //var restaurants = from r in db.Restaurants
-                //                  select r;
-
-                //var restaurants = from r in db.Restaurants
-                //                  where r.Cuisine == "Chinese"
-                //                  select r;
-
-                var restaurants = from r in db.Restaurants
-                                  orderby r.Cuisine
-                                  select r;
-
-                foreach (var res in restaurants)
-                {
-                    Console.WriteLine($"ID: {res.ResID}, Name: {res.Name}, Cuisine: {res.Cuisine}");
-                }
-            }
+            
         }
     }
 }
